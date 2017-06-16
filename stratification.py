5320b2c5d657b85cc801b9bf51a54d2c5956bab0
sarangof <sarangofc@gmail.com>
2017-06-16 11:12:30 -0400

    Randomization - no rounding.

--- stratification.py
+++ stratification.py
@@ -2,35 +2,36 @@ import pandas as pd
 from sklearn.model_selection import StratifiedShuffleSplit
 import numpy as np
 
-data = pd.read_csv('Randomization_short.csv')
+#data = pd.read_csv('Randomization_short.csv')
+data=pd.read_excel('NYU - Franklin JIYA to randomizecaseloads.xlsx')
+# Use regular expressions first to clean up the data
+
+data.dropna(axis=1,inplace=True)#,how='all')
+
+# Keep this in mind: https://github.com/scikit-learn/scikit-learn/blob/14031f6/sklearn/model_selection/_split.py#L1190
+
+sample_size=40
+#strat_columns=['Sex','Race','Risk']
+strat_columns = ['Gender','RISK Score']
+
+df = data.groupby(strat_columns).count().max(axis=1)
+# Create exception here
+df = df.reset_index()
+df[df.columns[-1]]
+
+# How to ensure sample size when rounding like this.
+df['Size'] = np.ceil(sample_size*(df[df.columns[-1]]/len(data)).values)
+
+# And then cut from the larger groups.
+i=0
+ind_list=np.array([])
+for index,comb in df.iterrows():
+	df_tmp = data[(data[comb[:-2].index]==comb[:-2].values).all(axis=1)]
+	print(df['Size'].iloc[i])
+	ind_list=np.append(ind_list,df_tmp.sample(n=df['Size'].iloc[i]).index.values)
+	i+=1
+
+data['Group'] = ["Intervention" if x in ind_list else "Control" for x in data.index]
 
 
 
-
-sample_size=3
-strat_columns=['Sex']
-
-# data['fake'] = 'CONTROL'
-# data['fake'][[5, 0, 7, 8]] = 'TEST'
-# data_list = []
-# for cols in strat_columns:
-#     data_pre = data[data[cols].isin(data[cols].value_counts()[data[cols].value_counts()<2].index)]
-#     data_list.append(data_pre)
-#     del(data_pre)
-# data_pre = pd.concat(data_list)
-sss = StratifiedShuffleSplit(n_splits=1, test_size=sample_size, random_state=2)
-#y = np.array(data[~data.isin(data_pre).T.any()][strat_columns])
-y = np.array(data[strat_columns])
-X = np.array([0] * y.shape[0])
-
-for train_index, test_index in sss.split(X, y):
-    print("TRAIN:", train_index, "TEST:", test_index)
-    X_train, X_test = X[train_index], X[test_index]
-    y_train, y_test = y[train_index], y[test_index]
-
-X = X.astype("string")
-X[train_index] = "Control"
-X[test_index] = "Test"
-data['Trial'] = X
-#(prefix, sep, suffix) = filename.rpartition('.')
-#data.to_csv(prefix + '_RCT.csv')
