f6929d4131db668982ee9ad4d206486938c5db63
sarangof <sarangofc@gmail.com>
2018-12-17 21:55:21 -0500

    In case something happens.

--- beta_functions.py
+++ beta_functions.py
@@ -7,21 +7,11 @@ import random
 from itertools import cycle
 
 def stratify(self):
-    """ 
-    Stratified random sampling
-    SPECIAL CASE, WHEN THERE IS ONLY ONE STRATUM PER INDIVIDUAL.
-    * The test_size = 1 should be greater or equal to the number of classes = 5
-    * Keep this in mind: https://github.com/scikit-learn/scikit-learn/blob/14031f6/sklearn/model_selection/_split.py#L1190
-    """
-
-    #data = pd.read_csv('Randomization_short.csv') #'NYU - Franklin JIYA to randomizecaseloads.xlsx'
     data_set = self.data
     selected_columns = self.strat_columns
     data_set.dropna(axis=1,inplace=True)#,how='all')
     data_set = data_set.apply(lambda x: x.astype(str).str.lower())
     n = np.ceil((self.sample_p/100.)*len(data_set))
-    #print(selected_columns)
-    #print(data_set.head())
 
     if "age" in self.strat_columns:
         data_set, age_copy, age_index = group_age(data_set)
@@ -31,10 +21,6 @@ def stratify(self):
         df = data_set.groupby(selected_columns).count().max(axis=1)
         #df = data_set.groupby(selected_columns).size() # Would this work?
         df = df.reset_index() # Create exception here
-
-        #print(n)
-        #print("df")
-        #print(df)
 
         # - How to ensure sample size when rounding like this.
         df['Size'] = np.ceil(n*(df[df.columns[-1]]/len(data_set)).values)
@@ -49,9 +35,6 @@ def stratify(self):
             else:
                 df.loc[rows,'Size'] -= 1
 
-        #print("df size")
-        #print(df)
-
         # And then cut from the larger groups.
         i=0
         ind_list=np.array([])
@@ -60,8 +43,6 @@ def stratify(self):
             ind_list = np.append(ind_list,df_tmp.sample(n=df['Size'].iloc[i]).index.values)
             i += 1
     else:
-        #print("index")
-        #print(n)
         ind_list = data_set.sample(n=int(n)).index.values
 
     data_set['group-rct'] = ["intervention" if x in ind_list else "control" for x in data_set.index]
@@ -106,19 +87,7 @@ def update_stratification(self):
     * Keep this in mind: https://github.com/scikit-learn/scikit-learn/blob/14031f6/sklearn/model_selection/_split.py#L1190
     """
 
-    #data = pd.read_csv('Randomization_short.csv') #'NYU - Franklin JIYA to randomizecaseloads.xlsx'
-
-    #data_set = pd.read_excel('Randomized/Originals Aug 22/NYU - Hamilton JIYA June 20,          AGE,RISK SCORE,Gender,Race-50_RCT.xlsx')
-    #data_new = pd.read_excel('To randomize Aug 22/Hamilton County cases to be randomized 8-15-17.xlsx')
-
     p = int(self.sample_p)/100. #percentual proportion
-    #print("p")
-    #print(self.sample_p)
-    #print("Existing data")
-    #print(self.data_rct.head())
-
-    #print("New data")
-    #print(self.data_new.head())
 
     data_set = self.data_rct#pd.read_excel(filename)
     data_set.dropna(axis=0,inplace=True,how='all',subset=data_set.columns[2:])
@@ -129,11 +98,6 @@ def update_stratification(self):
         pass
 
     data_new = self.data_new
-
-
-    #data_set=pd.read_excel('ICAN Texting Trial_Randomization List_6-27-18_V2|risklevel,age,sex,race,mentoring,sentencedordetained_2018-07-18_31_50_RCT.xlsx')
-    #data_new=pd.read_excel('ICAN Texting Trial_Randomization List_7-5-18.xlsx')
-
 
     data_new.dropna(axis=0,inplace=True,how='all',subset=data_new.columns[2:])
     data_new.dropna(axis=1,inplace=True,how='all')
@@ -154,14 +118,6 @@ def update_stratification(self):
     if 'age' in self.strat_columns:
         data_temp, age_copy, age_index = group_age(data_temp)
 
-    #print(selected_columns)
-
-    #print("RCT data:")
-    #print(data_set.head())
-
-    #print("New data:")
-    #print(data_new.head())
-
     #data_set = data_temp[data_temp.date != todaysdate] # seleccionar datos ya asignados
     data_set = data_temp[(data_temp['group-rct'].isin(['control','intervention']))] # seleccionar datos ya asignados
     print(data_set)
@@ -179,12 +135,6 @@ def update_stratification(self):
         print(selected_columns)
         df = data_temp.groupby(selected_columns).size().reset_index() # Number of individuals in each group
 
-        #print("label")
-        #print(label)
-
-        #print("CROSSTAB")
-        #print(pd.crosstab(data_set['group-rct'],[pd.Series(data_set[cols]) for cols in selected_columns]))
-
         label_pre = pd.crosstab(data_set['group-rct'],[pd.Series(data_set[cols]) for cols in selected_columns]).loc[label].reset_index() 
 
         # desired size
@@ -195,9 +145,6 @@ def update_stratification(self):
         else:
             print("ERROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOR")
         df['Size'] = np.ceil(n*(df[df.columns[-1]]/len(data_temp)).values) # number of individuals in the selected intervention that would make up for a balanced contribution to the covariates
-        # Trying to get a more exact sample size.
-        #print("ESTE VALOR")        
-        #print(n-df['Size'].sum())
 
         print('Grupos share')
         print(data_set_copy['group-rct'].value_counts(normalize=True))
@@ -211,9 +158,6 @@ def update_stratification(self):
 
         rows_delete = range(0,len(df))
         random.shuffle(rows_delete)
-
-        #print("DF BEFORE")
-        #print(df)
 
         previous_assignation = df['Size'].sum()
 
@@ -251,8 +195,6 @@ def update_stratification(self):
                         print("lo que falta "+str(n - initial_n - df['Size'].sum()))
                         print(added_ns)   
                         print(df)
-        #print("DF AFTER")
-        #print(df)
 
         print("Current assignation - after")
         print(df['Size'].sum())
@@ -267,8 +209,6 @@ def update_stratification(self):
         print("diff")
         print(diff)
 
-        #data_new = data_temp[data_temp.date==todaysdate] # what happens if an update happens on the same day?
-        #data_ = data_temp[data_temp['group-rct'].isnull()]
         data_new = data_temp[~(data_temp['group-rct'].isin(['control','intervention']))]
 
         # -- This cycle assigns groups at random given our corrected sizes.
@@ -277,34 +217,15 @@ def update_stratification(self):
             if assigned >= n - initial_n:
                 break
             else:
-                #print("comb")
-                #print(comb)
-                #print("subset")
-                #print(comb[:-4])
                 df_tmp = data_new[(data_new[comb[:-4].index]==comb[:-4].values).all(axis=1)]  # Combinations of factors.
-                #print("df_tmp")
-                #print(df_tmp)
-                #print(df_tmp.index)
                 sz = len(df_tmp)
                 ss = min([sz, df['Missing'].loc[index], diff]) # What I have vs. what I am missing, only god knows why diff is here.
-                #print("missing")
-                #print(df['Missing'].loc[index])
                 if ss > 0:
                     print(ss)
                     ind_list = np.append(ind_list, df_tmp.sample(n=int(ss)).index.values)
                     assigned += ss
                 else:
                     pass
-                #print("assigned")
-                #print(assigned)
-        print("Assigned")
-        print(assigned)
-        print("diff")
-        print(diff)
-        print("ind list pre")
-        print(ind_list)
-        print("len ind list pre")
-        print(len(set(ind_list)))
         # Arreglando esa colita
         if assigned < diff:
             print("Assigned are less assigned than the desired people to assign")
@@ -320,20 +241,7 @@ def update_stratification(self):
                 ind_list_b = elegible.index.values
                 ind_list   = np.append(ind_list,ind_list_b)
 
-        print("ind_list")
-        print(ind_list)
-        print("length")
-        print(len(ind_list))
-        # I can just randomly delete indices from the lists
-        #ind_list = list(pd.DataFrame(ind_list).sample(n))
-
-
-        #print(pd.DataFrame(ind_list)[0].value_counts())
-
         ind_list = map(int, ind_list)
-        
-        #print("data_new")
-        #print(data_new)
 
     else: 
         label_pre = data_set['group-rct'].value_counts().loc[label] 
@@ -347,18 +255,6 @@ def update_stratification(self):
             #n = np.ceil(p*len(data_temp))
         else:
             print("ERROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOR")
-        #df['Size'] = np.ceil(n*(df[df.columns[-1]]/len(data_temp)).values) # number of individuals in the selected intervention that would make up for a balanced contribution to the covariates
-        #print(data_set['group-rct'].value_counts().loc[label])
-        print(len(data_new))
-        print(n)
-        print(p)
-        print(label)
-        print(data_set['group-rct'].value_counts().loc[label])
-        ind_list = data_new.sample(int(n)-int(data_set['group-rct'].value_counts().loc[label])).index.values
-        #data_temp = data_new.append(data_set.ix[:, :])
-        # Trying to get a more exact sample size.
-        #print("ESTE VALOR")        
-        #print(n-df['Size'].sum())
 
     if label == 'control':
         data_new.loc[ind_list,'group-rct'] = "control"
@@ -368,12 +264,8 @@ def update_stratification(self):
                 print("malo")
                 print(x)
     else:
-        #data_new['group-rct'] = ["intervention" if x in ind_list else "control" for x in data_new.index]
         data_new.loc[ind_list,'group-rct'] = "intervention"
         data_new.loc[set(data_new.index.values ) - set(ind_list),'group-rct'] = "control"
-
-    #print("Value counts")
-    #print(data_new['group-rct'].value_counts())
 
     todaysdate = str(dt.datetime.today().date())
     data_new['batch'] = int(np.max(data_set.batch.value_counts().index.astype('int').values)) + int(1)
@@ -386,7 +278,6 @@ def update_stratification(self):
     if not self.pure_randomization_boolean: 
         self.name = self.filename1.rsplit("|")[0]+"|"+",".join(self.strat_columns)+'_'+todaysdate+'_'+str(int(len(self.total_data)))+'_'+str(int(int(self.sample_p)))+'_RCT'+'.xlsx'
         self.name_static = self.filename1.rsplit("|")[0]+"|"+",".join(self.strat_columns)+'_'+todaysdate+'_'+str(int(len(self.total_data)))+'_'+str(int(int(self.sample_p)))+'.xlsx'
-        #self.total_data['date'] = self.total_data['date'].dt.strftime('%M/%d/%Y')
     else:
         self.name = self.filename1.rsplit("|")[0]+"|"+str(self.pure_randomization_text)+'_'+todaysdate+'_'+str(int(len(self.total_data)))+'_'+str(int(int(self.sample_p)))+'_RCT'+'.xlsx'
         self.name_static = self.filename1.rsplit("|")[0]+"|"+str(self.pure_randomization_text)+'.xlsx'+'_'+todaysdate+'_'+str(int(len(self.total_data)))+'_'+str(int(int(self.sample_p)))+'.xlsx'
